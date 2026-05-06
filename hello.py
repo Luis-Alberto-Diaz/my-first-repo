10baaa0f3ae47a7060b0f0c67469d240812be33f
Luis Alberto Diaz <LuisDiazDev@proton.me>
2026-05-06 02:22:51 -0500

    expand hello.py with age and future age calculation

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
 name = input("What is your name? ")
-print(f"Hello, {name}! You just wrote your first Python program.")
+age = int(input("How old are you? "))
+print(f"Hello, {name}! You are {age} years old.")
+print(f"In 10 years you will be {age + 10}.")
